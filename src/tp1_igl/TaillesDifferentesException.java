9006ee960b57a7fd670ffce0bbdb4c0494cb7b31
Aziz <32706985+saiyenman@users.noreply.github.com>
2017-10-19 13:36:23 +0100

    Update TaillesDifferentesException.java

--- src/tp1_igl/TaillesDifferentesException.java
+++ src/tp1_igl/TaillesDifferentesException.java
@@ -12,6 +12,6 @@ package tp1_igl;
 public class TaillesDifferentesException extends Exception{
     
     public TaillesDifferentesException(){
-        System.out.println("Exception generee");
+        System.out.println("Exception gejjjjjneree");
     }
 }
